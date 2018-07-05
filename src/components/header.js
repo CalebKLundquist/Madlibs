4ada366f88369302bb1ff9a04da2aa9e3d0e245d
Caleb Lundquist <CalebKLundquist@gmail.com>
2018-07-05 17:10:15 +0300

    First Components: Header, Card, Input

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Bottega MadLibs</h1>
+            <p>There was once a man who lived in Peru</p>
+        </div>
+    )
+}
+
+export default Header;
